decab51e657d7e6ddf5da497522f5b248ec7cb11
Victor Narazas <victornarazas2005@gmail.com>
2025-06-18 16:55:36 -0500

    Proyecto iniciado

---
+++ LP2_NARAZAS_GARCIA_PRUEBA_T2/src/main/java/com/PruebaLP2/Lp2NarazasGarciaPruebaT2Application.java
@@ -0,0 +1,13 @@
+package com.PruebaLP2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lp2NarazasGarciaPruebaT2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lp2NarazasGarciaPruebaT2Application.class, args);
+	}
+
+}
---
+++ LP2_NARAZAS_GARCIA_PRUEBA_T2/src/test/java/com/PruebaLP2/Lp2NarazasGarciaPruebaT2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PruebaLP2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lp2NarazasGarciaPruebaT2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
